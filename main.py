64c6b998a7769d08ffef04c9c07346bef331d8e0
Juan Cruz <jc23729@gmail.com>
2020-12-01 18:31:12 +0000

    long form solution

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ tip = (int(input("How much tip would you like to give? 10, 12, 15 ?  ")))
 
 people = (int(input("How many people splitting the bill ? ")))
 
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+
 #short form
 final_amount = round(bill * (1 + tip / 100) / people, 2)
 
